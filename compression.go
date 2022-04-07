05ea40d580be60317467af2671b314dcbaa7ec58
Rakin Uddin <ruddin@atlassian.com>
2022-04-07 12:12:47 -0500

    updates compression producer to use new runsqs

--- compression.go
+++ compression.go
@@ -3,9 +3,12 @@ package compression
 import (
 	"bytes"
 	"compress/gzip"
+	"context"
 	"encoding/base64"
 
 	"github.com/asecurityteam/runsqs"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
 // CompressionSQSProducer compresses the message, and calls the wrapped
@@ -17,10 +20,10 @@ type CompressionSQSProducer struct {
 }
 
 // ProduceMessage produces a compressed, base64 encoded message to the configured sqs queue.
-func (producer *CompressionSQSProducer) ProduceMessage(message []byte) error {
+func (producer *CompressionSQSProducer) ProduceMessage(ctx context.Context, messageInput *sqs.SendMessageInput) error {
 	var bytes bytes.Buffer
 	gz := gzip.NewWriter(&bytes)
-	if _, err := gz.Write(message); err != nil {
+	if _, err := gz.Write([]byte(*messageInput.MessageBody)); err != nil {
 		return err
 	}
 	if err := gz.Close(); err != nil {
@@ -28,5 +31,13 @@ func (producer *CompressionSQSProducer) ProduceMessage(message []byte) error {
 	}
 	encodedBytes := make([]byte, base64.StdEncoding.EncodedLen(len(bytes.Bytes())))
 	base64.StdEncoding.Encode(encodedBytes, bytes.Bytes())
-	return producer.Wrapped.ProduceMessage(encodedBytes)
+	return producer.Wrapped.ProduceMessage(ctx, &sqs.SendMessageInput{
+		DelaySeconds:            messageInput.DelaySeconds,
+		MessageAttributes:       messageInput.MessageAttributes,
+		MessageDeduplicationId:  messageInput.MessageGroupId,
+		MessageGroupId:          messageInput.MessageGroupId,
+		MessageSystemAttributes: messageInput.MessageSystemAttributes,
+		QueueUrl:                messageInput.QueueUrl,
+		MessageBody:             aws.String(string(encodedBytes)),
+	})
 }
--- compression_test.go
+++ compression_test.go
@@ -1,8 +1,10 @@
 package compression
 
 import (
+	"context"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/golang/mock/gomock"
@@ -12,6 +14,7 @@ func TestCompression(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
+	mockContext := context.Background()
 	mockMessageProducer := NewMockSQSProducer(ctrl)
 
 	compressionProducer := CompressionSQSProducer{
@@ -20,8 +23,13 @@ func TestCompression(t *testing.T) {
 	originalMessage := "hello world!"
 	encodedCompressedMessage := "H4sIAAAAAAAA/8pIzcnJVyjPL8pJUQQEAAD//23CtAMMAAAA"
 
-	mockMessageProducer.EXPECT().ProduceMessage([]byte(encodedCompressedMessage)).Return(nil)
-	e := compressionProducer.ProduceMessage([]byte(originalMessage))
+	mockMessageProducer.EXPECT().ProduceMessage(mockContext, &sqs.SendMessageInput{
+		MessageBody: &encodedCompressedMessage,
+	}).Return(nil)
+
+	e := compressionProducer.ProduceMessage(mockContext, &sqs.SendMessageInput{
+		MessageBody: &originalMessage,
+	})
 	assert.Nil(t, e)
 
 }
